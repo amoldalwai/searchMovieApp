97511cf3ff4ea199602ad844c17e5fc06aeffc55
amoldalwai <amoldalwaiyt@gamil.com>
2020-09-09 00:19:50 +0530

    Updates

---
+++ precache-manifest.629f754334e6aea290206346aaf3be94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd71e241317449e2b674",
+    "url": "/searchMovieApp/static/css/main.489f4023.chunk.css"
+  },
+  {
+    "revision": "fd71e241317449e2b674",
+    "url": "/searchMovieApp/static/js/main.2827f352.chunk.js"
+  },
+  {
+    "revision": "3a73e4289ef32550da7d",
+    "url": "/searchMovieApp/static/js/runtime~main.e0567437.js"
+  },
+  {
+    "revision": "97a85c99969f18c7572b",
+    "url": "/searchMovieApp/static/css/2.0b7505f8.chunk.css"
+  },
+  {
+    "revision": "97a85c99969f18c7572b",
+    "url": "/searchMovieApp/static/js/2.170784c9.chunk.js"
+  },
+  {
+    "revision": "ec44ccf871de69f63b0577f7a32ea248",
+    "url": "/searchMovieApp/index.html"
+  }
+];
