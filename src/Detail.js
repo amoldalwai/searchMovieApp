f439370c64198023a51ecb6f2f93eda6d368e9a1
amoldalwai <amoldalwaiyt@gamil.com>
2020-09-09 00:12:50 +0530

    searchapp

---
+++ src/Detail.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Button from "@material-ui/core/Button";
+
+import Chip from "@material-ui/core/Chip";
+
+const Detail = (props) => {
+  const [open, setOpen] = React.useState(true);
+  const [poster, setposter] = useState();
+  const [year, setyear] = useState();
+  const [genre, setgenre] = useState();
+  const [plot, setplot] = useState();
+  const [title, settitle] = useState();
+  const [imdbrate, setimdbrate] = useState();
+  const [metascore, setmetascore] = useState();
+
+  axios
+    .get(`https://www.omdbapi.com/?apikey=4eb65943&i=${props.imval}`)
+    .then((res) => {
+      setposter(res.data.Poster);
+      setyear("(" + res.data.Year + ")");
+      setgenre(res.data.Genre);
+      setplot(res.data.Plot);
+      settitle(res.data.Title);
+      setimdbrate("Imdb: " + res.data.imdbRating);
+      setmetascore("Metascore: " + res.data.Metascore);
+    });
+
+  const handleClose = () => {
+    setOpen(false);
+    props.funsetdetailbool();
+  };
+
+  return (
+    <>
+      <Dialog
+        fullWidth="true"
+        maxWidth="sm"
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        className="moviePosterDialog"
+      >
+        <img
+          src={poster}
+          maxWidth="sm"
+          className="moviePosterDetail"
+          alt="Poster"
+        />
+        <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            <span style={{ fontSize: "12px" }}>
+              {genre} <b>{year}</b>
+            </span>
+            <Chip
+              label={metascore}
+              color="secondary"
+              style={{ float: "right" }}
+            />
+            <Chip label={imdbrate} color="primary" style={{ float: "right" }} />
+            <hr />
+            <span>{plot}</span>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+export default Detail;
---
+++ src/List.js
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+import axios from "axios";
+import SearchIcon from "@material-ui/icons/Search";
+import Fab from "@material-ui/core/Fab";
+import Footer from "./Footer";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+
+import Detail from "./Detail";
+
+const List = () => {
+  const [stext, setstext] = useState("batman");
+  const [result, setresult] = useState([]);
+  const [detailbool, setdetailbool] = useState(false);
+  const [imval, setimval] = useState();
+
+  const showChange = (event) => {
+    setstext(event.target.value);
+  };
+  const show = () => {
+    axios
+      .get(`https://www.omdbapi.com/?i=tt3896198&apikey=4eb65943&s=${stext}`)
+      .then((res) => {
+        setresult(res.data.Search.map((p) => p));
+      });
+  };
+  function showDetail(i) {
+    setdetailbool(true);
+    setimval(i);
+  }
+  //to toggle boolean
+  function funsetdetailbool() {
+    setdetailbool(false);
+  }
+
+  return (
+    <>
+      <center>
+        <div className=" navbar-dark bg-dark">
+          <br />
+          <input
+            type="text"
+            value={stext}
+            onChange={showChange}
+            placeholder="Search movies/series"
+            style={{
+              height: "57px",
+              width: "300px",
+              borderRadius: "5px",
+              paddingLeft: "30px",
+              color: "white",
+              background: "#212121",
+              border: "2px",
+            }}
+          />
+          <Fab
+            color="primary"
+            aria-label="add"
+            style={{
+              background: "#b71c1c",
+              borderRadius: "7px",
+            }}
+          >
+            <SearchIcon onClick={show} />
+          </Fab>
+          <br />
+          <br />
+        </div>
+        <br />
+        {/* <Display result={result}/> */}
+        {result.map((p) => (
+          <div key={p.imdbID} onClick={() => showDetail(p.imdbID)}>
+            <Card className="movieCard">
+              <CardActionArea>
+                <img className="moviePoster" src={p.Poster} alt="poster" />
+                <div style={{ paddingTop: "5px" }}>
+                  <h6>{p.Title}</h6>
+                </div>
+              </CardActionArea>
+            </Card>
+          </div>
+        ))}
+        {detailbool ? (
+          <Detail imval={imval} funsetdetailbool={funsetdetailbool} />
+        ) : null}
+      </center>
+      <Footer />
+    </>
+  );
+};
+export default List;
